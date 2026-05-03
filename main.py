fd367ddca6a757a831f5996b639541b2fce1dc23
jackbrannan0 <jackbrannan007@icloud.com>
2026-05-03 16:41:27 +0100

    basic function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 app = Flask(__name__)
 
@@ -6,23 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template('/index.html')
 
-@app.route('/metar')
+@app.route('/metar', methods=['GET', 'POST'])
 def metar():
-    return render_template('/metar.html')
+    if request.method == 'POST':
+        
+        try:
+            airport_icao = request.form.get('icao_input')
+            if not airport_icao:
+                return render_template('/metar.html', error="Airport code is null")
+            response = requests.get(f"https://aviationweather.gov/api/data/metar?ids={airport_icao}&format=json")
+            data = response.json()
+            if not data:
+                return render_template('/metar.html', error="No data returned")
+            raw_metar = data[0]['rawOb']
+            return render_template('/metar.html', raw_metar=raw_metar)
+        except:
+            return render_template('/metar.html', error="Generic error")
+        
+
+    elif request.method == 'GET':
+
+        return render_template('/metar.html')
+    
 
 @app.route('/airportinfo')
 def airportInfo():
     return render_template('/airportinfo.html')
 
-@app.route('/metar')
-def getMetar():
-    try:
-        response = requests.get("https://aviationweather.gov/api/data/metar?ids=EGSS&format=json")
-        data = response.json()
-        raw_metar = data[0]['rawOb']
-        return render_template(raw_metar=raw_metar)
-    except:
-        return ("Data Invalid", 400)  
 
 
 if __name__ == "__main__":
